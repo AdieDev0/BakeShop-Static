44ead2609b5dfef1a1adea0c8daeeb8fcaf5cde2
AdieDev0 <devutella@gmail.com>
2024-12-24 02:33:09 +0800

    decoration and underline-offset[px]

--- cake/src/Components/Navbar.jsx
+++ cake/src/Components/Navbar.jsx
@@ -1,25 +1,39 @@
 import React from "react";
-import cake from '../assets/Cake.png'
+import cake from "../assets/Cake.png";
 import { BsSearchHeart } from "react-icons/bs";
 
 const Navbar = () => {
   return (
-    <div className="z-50 sticky">
+    <div className="z-50 sticky bg-pink-400 px-52 py-4 w-full shadow-md">
       <div className="flex justify-between">
         {/* LOGO AND BAKESHOP NAME */}
-        <div className="gap-5 flex items-center">
-            <img src={cake} alt="/" className="size-12" />
-            <h3>ButterCup</h3>
+        <div className="gap-3 flex items-center">
+          <img src={cake} alt="/" className="size-12 cursor-pointer" />
+          <h3 className="text-2xl text-white font-medium font-Pacifico cursor-pointer">
+            ButterCup
+          </h3>
         </div>
         {/* NAVIGATION */}
         <div className="flex gap-5 items-center">
-            <h2 className="text-lg">MENU</h2>
-            <h2 className="text-lg">LOCATION</h2>
-            <h2 className="text-lg">PROMOS</h2>
-            <h2 className="text-lg">FRANCHISING</h2>
-            <h2 className="text-lg">CAREERS</h2>
-            <h2 className="text-lg">ORDER ONLINE</h2>
-            <BsSearchHeart className="cursor-pointer size-7"/> 
+          <h2 className="text-md text-white font-semibold decoration-pink-200 decoration-2 hover:underline underline-offset-[30px] duration-200 transition font-Josefin-Sans cursor-pointer">
+            MENU
+          </h2>
+          <h2 className="text-md text-white font-semibold decoration-pink-200 decoration-2 hover:underline underline-offset-[30px] duration-200 transition  font-Josefin-Sans cursor-pointer">
+            LOCATION
+          </h2>
+          <h2 className="text-md text-white font-semibold decoration-pink-200 decoration-2 hover:underline underline-offset-[30px] duration-200 transition font-Josefin-Sans cursor-pointer">
+            PROMOS
+          </h2>
+          <h2 className="text-md text-white  font-semibold decoration-pink-200 decoration-2 hover:underline underline-offset-[30px] duration-200 transition font-Josefin-Sans cursor-pointer">
+            FRANCHISING
+          </h2>
+          <h2 className="text-md text-white font-semibold decoration-pink-200 decoration-2 hover:underline underline-offset-[30px] duration-200 transition font-Josefin-Sans cursor-pointer">
+            CAREERS
+          </h2>
+          <h2 className="text-md text-white font-semibold decoration-pink-200 decoration-2 hover:underline underline-offset-[30px] duration-200 transition font-Josefin-Sans cursor-pointer">
+            ORDER ONLINE
+          </h2>
+          <BsSearchHeart className="cursor-pointer size-7 text-white ml-5" />
         </div>
       </div>
     </div>
--- cake/tailwind.config.js
+++ cake/tailwind.config.js
@@ -8,7 +8,8 @@ export default {
     extend: {
       fontFamily:{
         "Josefin-Sans": ["Josefin Sans"],
-      }
+        "Pacifico": ["Pacifico"],
+      },
     },
   },
   plugins: [],
