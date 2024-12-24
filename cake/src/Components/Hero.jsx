43df2263f9c7b0a432d91011e0fd5b52ad6f7e9b
AdieDev0 <devutella@gmail.com>
2024-12-24 15:08:46 +0800

    Add Marquee

--- cake/src/Components/Hero.jsx
+++ cake/src/Components/Hero.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import heroBg from "../assets/HeroBg.jpg";
 import { FaRegHandPointRight } from "react-icons/fa";
 import { motion } from "framer-motion";
 const Hero = () => {
+  // IMAGE SLIDER DATA
+  const slides = [
+    { URL: "https://wallpaperaccess.com/full/7793868.jpg" },
+    { URL: "https://wallpaperaccess.com/full/7793880.jpg" },
+    { URL: "https://wallpaperaccess.com/full/7793887.jpg" },
+    { URL: "https://wallpaperaccess.com/full/6172149.jpg" },
+    { URL: "https://wallpaperaccess.com/full/5788695.jpg" },
+    { URL: "https://wallpaperaccess.com/full/6450201.jpg" },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    const newIndex = currentIndex === slides.length - 1 ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  // Auto-slide functionality
+  useEffect(() => {
+    const intervalId = setInterval(nextSlide, 3000); // Change slide every 3 seconds
+
+    return () => clearInterval(intervalId); // Cleanup interval on component unmount
+  }, [currentIndex]);
+
   return (
     <div className="relative">
+        <motion.div
+        style={{ backgroundImage: `url(${slides[currentIndex].URL})` }}
+        className="relative w-full h-[350px] sm:h-[590px] bg-center bg-cover bg-no-repeat duration-500"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      ></motion.div>
       <img
         src={heroBg}
         alt="background"
@@ -19,7 +50,7 @@ const Hero = () => {
         </h1>
 
         <button className="flex gap-4 text-xl md:text-4xl font-bold text-red-400 hover:text-white items-center px-5 md:px-10 py-3 md:py-5 bg-white hover:bg-red-400 duration-150 p-2 rounded-full">
-          TAP TO ORDER <FaRegHandPointRight/>
+          TAP TO ORDER <FaRegHandPointRight />
         </button>
       </div>
     </div>
