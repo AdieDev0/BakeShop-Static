90e9f1466c7eb0b8f48fb4116444de61766a853e
AdieDev0 <devutella@gmail.com>
2024-12-25 14:00:02 +0800

    FeatureSelection

--- cake/src/App.jsx
+++ cake/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
+import FeatureSelection from "./Components/FeatureSelection";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Hero/>
+      <Hero />
+      <FeatureSelection />
     </div>
   );
 };
---
+++ cake/src/Components/FeatureSelection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FeatureSelection = () => {
+  return (
+    <div>FeatureSelection</div>
+  )
+}
+
+export default FeatureSelection
