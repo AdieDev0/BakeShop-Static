ff2682ba94a103f8ea763c33399da0ceb7aad287
AdieDev0 <devutella@gmail.com>
2024-12-25 14:58:21 +0800

    3 Cards

--- cake/src/App.jsx
+++ cake/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import FeatureSelection from "./Components/FeatureSelection";
+import Cards from "./Components/Cards";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <FeatureSelection />
+      <Cards/>
     </div>
   );
 };
---
+++ cake/src/Components/Cards.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div className='bg-white p-20'>
+        <div className='flex justify-between'>
+            {/* IMAGE 1 */}
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <img src="" alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Cards
