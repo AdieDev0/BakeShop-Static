0aee973991ea8fca54322043665a1492c70ed8c2
AdieDev0 <devutella@gmail.com>
2024-12-25 16:06:05 +0800

    Back Later

--- cake/src/Components/Cards.jsx
+++ cake/src/Components/Cards.jsx
@@ -1,22 +1,45 @@
-import React from 'react'
+import React from "react";
+import birthdayCake from "../assets/cards/BirthdayCake.jpg";
+import foodShop from "../assets/cards/foodShop.jpg";
+import party from "../assets/cards/party.jpg";
+import cakeIcon from "../assets/cards/CakeIcon.png";
 
 const Cards = () => {
   return (
-    <div className='bg-white p-20'>
-        <div className='flex justify-between'>
-            {/* IMAGE 1 */}
-            <div>
-                <img src="" alt="" />
-            </div>
-            <div>
-                <img src="" alt="" />
-            </div>
-            <div>
-                <img src="" alt="" />
-            </div>
-        </div>
-    </div>
-  )
-}
+    <div className="bg-white p-20">
+      <div className="flex gap-5 justify-between">
+        {/* IMAGE 1 */}
+        <div className="size-96 relative cursor-pointer group">
+  <img
+    src={birthdayCake}
+    alt="Birthday Cake"
+    className="object-cover inset-0 w-full h-full absolute rounded-xl group-hover:opacity-40 transition-opacity duration-200"
+  />
+  <div className="absolute inset-0 w-full h-full flex flex-col items-center justify-center bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity duration-200 rounded-xl">
+    <img src={cakeIcon} alt="Cake Icon" className="w-24 h-24 mb-2" />
+    <h3 className="text-white">Bakeshop</h3>
+  </div>
+</div>
 
-export default Cards
+        {/* IMAGE 2 */}
+        <div className="border size-96 relative">
+          <img
+            src={foodShop}
+            alt=""
+            className="object-cover inset-0 w-full h-full absolute"
+          />
+        </div>
+        {/* IMAGE 3 */}
+        <div className="border size-96 relative">
+          <img
+            src={party}
+            alt=""
+            className="object-cover inset-0 w-full h-full absolute"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
