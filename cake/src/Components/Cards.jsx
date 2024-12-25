d655f1d5f836885c48de05a569590471c24604c1
AdieDev0 <devutella@gmail.com>
2024-12-26 01:29:07 +0800

    Add later

--- cake/src/Components/Cards.jsx
+++ cake/src/Components/Cards.jsx
@@ -10,19 +10,19 @@ const Cards = () => {
       <div className="flex gap-5 justify-between">
         {/* IMAGE 1 */}
         <div className="size-96 relative cursor-pointer group">
-  <img
-    src={birthdayCake}
-    alt="Birthday Cake"
-    className="object-cover inset-0 w-full h-full absolute rounded-xl group-hover:opacity-40 transition-opacity duration-200"
-  />
-  <div className="absolute inset-0 w-full h-full flex flex-col items-center justify-center bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity duration-200 rounded-xl">
-    <img src={cakeIcon} alt="Cake Icon" className="w-24 h-24 mb-2" />
-    <h3 className="text-white">Bakeshop</h3>
-  </div>
-</div>
+          <img
+            src={birthdayCake}
+            alt="Birthday Cake"
+            className="object-cover inset-0 w-full h-full absolute rounded-xl group-hover:opacity-40 transition-opacity duration-200"
+          />
+          <div className="absolute inset-0 w-full h-full flex flex-col items-center justify-center bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity duration-200 rounded-xl">
+            <img src={cakeIcon} alt="Cake Icon" className="w-24 h-24 mb-2" />
+            <h3 className="text-white">Bakeshop</h3>
+          </div>
+        </div>
 
         {/* IMAGE 2 */}
-        <div className="border size-96 relative">
+        <div className="border size-96 relative cursor-pointer group">
           <img
             src={foodShop}
             alt=""
@@ -30,7 +30,7 @@ const Cards = () => {
           />
         </div>
         {/* IMAGE 3 */}
-        <div className="border size-96 relative">
+        <div className="border size-96 relative cursor-pointer group">
           <img
             src={party}
             alt=""
