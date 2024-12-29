cf63ec36ecd668b4d7a363e953d710a402563810
AdieDev0 <devutella@gmail.com>
2024-12-30 01:21:16 +0800

    Setup

--- cake/src/Components/CreditCards.jsx
+++ cake/src/Components/CreditCards.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const CreditCards = () => {
   return (
-    <div>CreditCards</div>
+    <div>
+        <div className='flex justify-between'>
+            <img src="" alt="" />
+            <div>
+                <p>Please be advised that the Credit Cards will be discontinued on October 31, 2024c</p>
+                <button>LEARN MORE</button>
+            </div>
+
+        </div>
+    </div>
   )
 }
 
