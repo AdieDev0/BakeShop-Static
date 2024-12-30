676257927f6e292e880141c2736f5cd173f4a56b
AdieDev0 <devutella@gmail.com>
2024-12-30 11:07:00 +0800

    Enhance Later

--- cake/src/Components/Footer.jsx
+++ cake/src/Components/Footer.jsx
@@ -4,15 +4,21 @@ import { FaInstagram } from "react-icons/fa6";
 import { FaFacebookSquare } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div className="bg-white/50">
+    <div className="bg-white/50 min-h-screen">
       <div>
-        <ul className="flex gap-5">
-          <li>OUR STORY</li>
-          <li>PRIVACY POLICY</li>
-          <li>CONTACCT US</li>
-          <IoLogoGithub />
-          <FaInstagram />
-          <FaFacebookSquare />
+        <ul className="flex gap-5 items-center">
+          <li className="text-md text-white font-semibold decoration-red-200 decoration-2 hover:underline underline-offset-[30px] duration-200 transition font-Josefin-Sans cursor-pointer">
+            OUR STORY
+          </li>
+          <li className="text-md text-white font-semibold decoration-red-200 decoration-2 hover:underline underline-offset-[30px] duration-200 transition font-Josefin-Sans cursor-pointer">
+            PRIVACY POLICY
+          </li>
+          <li className="text-md text-white font-semibold decoration-red-200 decoration-2 hover:underline underline-offset-[30px] duration-200 transition font-Josefin-Sans cursor-pointer">
+            CONTACT US
+          </li>
+          <IoLogoGithub className="text-md text-white font-semibold decoration-red-200 decoration-2 hover:underline underline-offset-[30px] duration-200 transition font-Josefin-Sans cursor-pointer" />
+          <FaInstagram className="text-md text-white font-semibold decoration-red-200 decoration-2 hover:underline underline-offset-[30px] duration-200 transition font-Josefin-Sans cursor-pointer" />
+          <FaFacebookSquare className="text-md text-white font-semibold decoration-red-200 decoration-2 hover:underline underline-offset-[30px] duration-200 transition font-Josefin-Sans cursor-pointer" />
         </ul>
       </div>
     </div>
