20e2f9befd1ff482d3a41177cc191a0d8c88dc35
AdieDev0 <devutella@gmail.com>
2024-12-31 13:08:15 +0800

    Comments

--- cake/src/Components/Careers/Careers.jsx
+++ cake/src/Components/Careers/Careers.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Careers = () => {
   return (
     <>
-    
-    <div className='w-full h-auto bg-gradient-to-b from-red-300 via-red-500 to-pink-500 p-5'>
-        <div className='bg-white p-5 rounded-xl'>
-            
+      <div className="w-full h-auto bg-gradient-to-b from-red-300 via-red-500 to-pink-500 px-20 py-10">
+        {/* Body */}
+        <div className="bg-white p-5 rounded-xl justify-center text-center items-center">
+          <h1 className="text-black font-semibold font-Josefin-Sans text-2xl mb-5">
+            CAREERS
+          </h1>
+          <p className="w-80 mx-auto text-black/70 text-sm font-Josefin-Sans">
+            Experience sweet success with our growing family and be part of the
+            country's number one bakeshop today!
+          </p>
+
+          {/* image */}
         </div>
-    </div>
+      </div>
     </>
+  );
+};
 
-  )
-}
-
-export default Careers
+export default Careers;
