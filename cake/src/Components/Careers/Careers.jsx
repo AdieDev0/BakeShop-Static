5188e30d77e264927ac4ef34fed083fd10db3733
AdieDev0 <devutella@gmail.com>
2024-12-31 12:28:31 +0800

    Set Up Careers

--- cake/src/Components/Careers/Careers.jsx
+++ cake/src/Components/Careers/Careers.jsx
@@ -4,8 +4,8 @@ const Careers = () => {
   return (
     <>
     
-    <div>
-        <div>
+    <div className='w-full h-auto bg-gradient-to-b from-red-300 via-red-500 to-pink-500 p-5'>
+        <div className='bg-white p-5 rounded-xl'>
             
         </div>
     </div>
