d37e7707e6e1b5fb3c4d34f32ad8207388f5e63b
AdieDev0 <devutella@gmail.com>
2024-12-31 13:10:19 +0800

    Update Folder Careers

--- cake/src/Components/Careers/Careers.jsx
+++ cake/src/Components/Careers/Careers.jsx
@@ -5,7 +5,7 @@ const Careers = () => {
     <>
       <div className="w-full h-auto bg-gradient-to-b from-red-300 via-red-500 to-pink-500 px-20 py-10">
         {/* Body */}
-        <div className="bg-white p-5 rounded-xl justify-center text-center items-center">
+        <div className="bg-white py-10 px-60 rounded-xl justify-center text-center items-center">
           <h1 className="text-black font-semibold font-Josefin-Sans text-2xl mb-5">
             CAREERS
           </h1>
@@ -15,6 +15,11 @@ const Careers = () => {
           </p>
 
           {/* image */}
+          <img src="" alt="" />
+          <div>
+            <img src="" alt="" />
+            <img src="" alt="" />
+          </div>
         </div>
       </div>
     </>
