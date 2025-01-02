5a358a404fa095357cf0c218e0910ac693d794fa
AdieDev0 <devutella@gmail.com>
2025-01-02 18:39:02 +0800

    Add style in text

--- cake/src/Components/Franchising/Franchise.jsx
+++ cake/src/Components/Franchising/Franchise.jsx
@@ -5,14 +5,15 @@ const Franchise = () => {
     <div className="bg-gradient-to-b from-red-300 via-red-500 to-pink-500 min-h-screen py-10 px-5 md:px-20 font-sans">
       <div className="bg-white py-10 px-5 md:px-20 rounded-xl max-w-7xl mx-auto shadow-lg">
         {/* TEXT BODY */}
-        <div className="text-center justify-center">
-          <h1>Franchise</h1>
-          <p>
+        <div className="text-center justify-center mx-80">
+          <h1 className="text-black font-Josefin-Sans text-2xl font-semibold mb-5">FRANCHISING</h1>
+          <p className="text-xs text-black/70 font-Josefin-Sans">
             BUTTERCUP traces its roots to the collaboration and complementary
             talents of the women, whose collective love for good food fueled
             what has been transformed into the global enterprise today.
           </p>
         </div>
+        {/* image and text */}
       </div>
     </div>
   );
