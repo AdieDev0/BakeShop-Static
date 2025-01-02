4824a321ed55e86cb68e64c8c219147b78744310
AdieDev0 <devutella@gmail.com>
2025-01-02 18:33:34 +0800

    text

--- cake/src/Components/Franchising/Franchise.jsx
+++ cake/src/Components/Franchising/Franchise.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Franchise = () => {
   return (
-    <div>Franchise</div>
-  )
-}
+    <div className="bg-gradient-to-b from-red-300 via-red-500 to-pink-500 min-h-screen py-10 px-5 md:px-20 font-sans">
+      <div className="bg-white py-10 px-5 md:px-20 rounded-xl max-w-7xl mx-auto shadow-lg">
+        {/* TEXT BODY */}
+        <div className="text-center justify-center">
+          <h1>Franchise</h1>
+          <p>
+            BUTTERCUP traces its roots to the collaboration and complementary
+            talents of the women, whose collective love for good food fueled
+            what has been transformed into the global enterprise today.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Franchise
+export default Franchise;
