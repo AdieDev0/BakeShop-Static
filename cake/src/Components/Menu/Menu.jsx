91424e959ed774f0c6caafe02ddb1db7866d4857
AdieDev0 <devutella@gmail.com>
2025-01-03 00:23:23 +0800

    Menu

--- cake/src/Components/Menu/Menu.jsx
+++ cake/src/Components/Menu/Menu.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Menu = () => {
   return (
-    <div>Menu</div>
-  )
-}
+    <div className="bg-gradient-to-b from-red-300 via-red-500 to-pink-500 min-h-screen py-10 px-5 md:px-20 font-sans">
+      <div className="bg-white py-10 px-5 md:px-10 lg:px-20 rounded-xl max-w-7xl mx-auto shadow-lg">
+        
+      </div>
+    </div>
+  );
+};
 
-export default Menu
+export default Menu;
