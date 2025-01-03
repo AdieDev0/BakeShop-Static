a35032ca8993dd638b05728268b91e6f1faf045f
AdieDev0 <devutella@gmail.com>
2025-01-03 16:11:19 +0800

    Update Later

--- cake/src/Components/OrderOnline/OrderOnline.jsx
+++ cake/src/Components/OrderOnline/OrderOnline.jsx
@@ -1,47 +1,37 @@
 import React from "react";
+import Marquee from "react-fast-marquee";
+import { motion } from "framer-motion";
+import { FaRegHandPointRight } from "react-icons/fa";
+// DATA IMAGE
+import Triple from "../../assets/Order Online/TripleChoco.jpg";
 import Cake from "../../assets/Order Online/R.jpg";
 import Cherry from "../../assets/Order Online/cherry.jpg";
+import DarkChoco from "../../assets/Order Online/DarkChoco.jpg";
+import sisig from "../../assets/Order Online/sisig.jpg";
+import fiesta from "../../assets/Order Online/fiesta.jpg";
+import shanghai from "../../assets/Order Online/shanghai.jpg";
+import icedCoffee from "../../assets/Order Online/icedCoffee.jpg";
+import Mocha from "../../assets/Order Online/Mocha.jpg";
+import Cream from "../../assets/Order Online/Cream.jpg";
 const OrderOnline = () => {
   return (
     <div>
-      <div className="carousel">
-        <div className="carousel-item">
-          <img src={Cake} alt="Burger" className="w-72" />
+      <Marquee autoFill>
+        <div className="flex relative">
+          <img src={Cake} alt="/" className="w-80" />
+          <img src={icedCoffee} alt="/" className="w-80" />
+          <img src={sisig} alt="/" className="w-80" />
+          <img src={Cherry} alt="/" className="w-80" />
+          <img src={Mocha} alt="/" className="w-80" />
+          <img src={fiesta} alt="/" className="w-80" />
+          <img src={Triple} alt="/" className="w-80" />
+          <img src={shanghai} alt="/" className="w-80" />
+          <img src={Cream} alt="/" className="w-80" />
+          <img src={DarkChoco} alt="/" className="w-80" />
+          {/* BACKGROUND COLOR */}
+          <div className="bg-black/40 absolute inset-0"></div>
         </div>
-        <div className="carousel-item">
-          <img src={Cherry} alt="Burger" className="w-72" />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
-            alt="Burger"
-          />
-        </div>
-      </div>
+      </Marquee>
     </div>
   );
 };
