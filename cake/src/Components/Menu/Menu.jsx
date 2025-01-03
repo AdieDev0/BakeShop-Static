e6cb1ef96c1c80c712263540ab3e62a45888bf1d
AdieDev0 <devutella@gmail.com>
2025-01-03 14:22:18 +0800

    Footer

--- cake/src/Components/Menu/Menu.jsx
+++ cake/src/Components/Menu/Menu.jsx
@@ -78,6 +78,47 @@ const Menu = () => {
             ))}
           </div>
         </div>
+        {/* Footer */}
+        <footer className="footer text-base-content p-10 mt-10">
+          <nav>
+            <h6 className="footer-title text-xl text-black">Services</h6>
+            <a className="link link-hover text-black">Branding</a>
+            <a className="link link-hover text-black">Design</a>
+            <a className="link link-hover text-black">Marketing</a>
+            <a className="link link-hover text-black">Advertisement</a>
+          </nav>
+          <nav>
+            <h6 className="footer-title text-xl text-black">Company</h6>
+            <a className="link link-hover text-black">About us</a>
+            <a className="link link-hover text-black">Contact</a>
+            <a className="link link-hover text-black">Jobs</a>
+            <a className="link link-hover text-black">Press kit</a>
+          </nav>
+          <nav>
+            <h6 className="footer-title text-xl text-black">Legal</h6>
+            <a className="link link-hover text-black">Terms of use</a>
+            <a className="link link-hover text-black">Privacy policy</a>
+            <a className="link link-hover text-black">Cookie policy</a>
+          </nav>
+          <form>
+            <h6 className="footer-title text-xl text-black">Newsletter</h6>
+            <fieldset className="form-control w-80">
+              <label className="label">
+                <span className="label-text text-black">
+                  Enter your email address
+                </span>
+              </label>
+              <div className="join">
+                <input
+                  type="text"
+                  placeholder="ButterCup@site.com"
+                  className="input input-bordered join-item bg-white placeholder-black/60 border-2"
+                />
+                <button className="btn join-item">Subscribe</button>
+              </div>
+            </fieldset>
+          </form>
+        </footer>
       </div>
     </div>
   );
